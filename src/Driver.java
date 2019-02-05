5924aaf51603750e04c6cf6824bd27c17d3f8003
Logan Davis <Logand222@gmail.com>
2019-02-05 16:43:03 -0700

    Added memory dump file EVERYWHERE

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,22 @@
+import java.io.*;
+import java.util.*;
+
 public class Driver
 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Joel is lovely as a person");
     }
     
+
+
+    public String readFileAsString(String fileName) {
+        String text = "";
+        try {
+          text = new String(Files.readAllBytes(Paths.get("memorydump.dmp")));
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+    
+        return text;
+      }
 }
